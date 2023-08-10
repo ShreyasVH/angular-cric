e6818a67dab0f799579619f0183736c5e08b848a
ShreyasVH <shreyas.deve.1@gmail.com>
2023-08-10 20:54:13 +0530

    using env variables for api endpoint

--- src/app/endpoints/series.ts
+++ src/app/endpoints/series.ts
@@ -1,7 +1,6 @@
 import { get } from '../utils/api';
 import { environment } from '../../environments/environments'
-const endpoint = 'https://cric.dotnet.com';
-console.log(environment)
+const endpoint = environment.apiUrl;
 
 export const getById = async (id: number): Promise<any> => {
     const url = endpoint + '/cric/v1/series/' + id;
--- src/app/endpoints/tours.ts
+++ src/app/endpoints/tours.ts
@@ -1,5 +1,6 @@
 import { get } from '../utils/api';
-const endpoint = 'https://cric.dotnet.com';
+import { environment } from '../../environments/environments';
+const endpoint = environment.apiUrl;
 
 export const getToursForYear = async (year: number, page: number, pageSize: number): Promise<any> => {
     const url = endpoint + '/cric/v1/tours/year/' + year + '?page=' + page + '&limit=' + pageSize;
