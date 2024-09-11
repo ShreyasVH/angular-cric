006c5ed376608d516e492e516a0f6732d3e029b4
ShreyasVH <shreyas.deve.1@gmail.com>
2024-09-11 16:21:58 +0530

    ensure environments folder

--- config-env.ts
+++ config-env.ts
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
 const values = `
 export const environment = { 
     production: ${JSON.parse(String(process.env['PRODUCTION'] ?? false))}, 
     apiUrl: '${process.env['API_ENDPOINT']}'
 };`
+
+const environmentsFolderPath = path.resolve(__dirname, './src/environments')
+if (!fs.existsSync(environmentsFolderPath)) {
+    fs.mkdirSync(environmentsFolderPath);
+}
 
 fs.writeFileSync('./src/environments/environments.ts', values);
