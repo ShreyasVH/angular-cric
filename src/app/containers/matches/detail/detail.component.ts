f096c55a371cec7120499f8d349c4b63ecd73a93
ShreyasVH <shreyas.deve.1@gmail.com>
2026-04-06 18:01:53 +0530

    handling player click

--- src/app/containers/matches/detail/detail.component.ts
+++ src/app/containers/matches/detail/detail.component.ts
@@ -32,7 +32,7 @@ export class MatchDetailComponent {
     }
 
     handlePlayerClick(playerId: number) {
-        console.log(playerId);
+        this.router.navigate(['/players/detail'], { queryParams: { id: playerId } });
     }
 
     getTeams() {
