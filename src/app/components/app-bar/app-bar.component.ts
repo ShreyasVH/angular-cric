bb47012fc9aabb1939336f01d0865cc15ac848a3
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-08-17 12:30:24 +0530

    support dark theme
    * intermediate commit
    
    * hiding dropdown
    
    * support for dark theme

--- src/app/components/app-bar/app-bar.component.ts
+++ src/app/components/app-bar/app-bar.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
 import SearchSelect from '../search-select/search-select';
 import { Router } from "@angular/router";
+import ThemeSelector from '../theme-selector/theme-selector';
 
 @Component({
     selector: 'app-bar',
     templateUrl: './app-bar.component.html',
     styleUrls: ['./app-bar.component.css'],
-    imports: [MatIconModule, MatToolbarModule, MatButtonModule, SearchSelect]
+    imports: [MatIconModule, MatToolbarModule, MatButtonModule, SearchSelect, ThemeSelector]
 })
 export class AppBarComponent {
     constructor(private router: Router) { }
